a9ce868bc6b54b3888beb9b1c986358db1bc8d9a
Kumar Abhinav <kumar.abhinav@aline-consulting.com>
2016-09-27 16:26:39 +0530

    changes

--- app/app.js
+++ app/app.js
@@ -1,34 +1,24 @@
-var calc = (function() {
-    var x = 3;
-
-    return {
-        add: function(n1, n2) {
-            return n1 + n2
-        },
-        mul: function(n1, n2) {
-            return n1 * n2
-        }
-    }
-})();
-
-var car = (function() {
-    var wheels = 4,
+var wheels = 4,
         speed = 10;
 
-     return {
+
+var car =  {
      	accelarate: function () {
      		speed+= 10;
      	},
-     	move: function () {
+     	move: function () {	
      		console.log('moving');
      	},
      	showSpeedoMeter: function () {
      		console.log('SPEED:: '+speed+'KMPH');
      	}
-     }
-})();
+     };
+
+     
+
 
 car.move();
 car.showSpeedoMeter();
 car.accelarate();
-car.showSpeedoMeter();
+car.showSpeedoMeter();	
+
