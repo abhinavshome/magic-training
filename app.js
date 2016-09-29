02393b476cf5af70e4e486621ed9abd66b9959b5
Kumar Abhinav <kumar.abhinav@aline-consulting.com>
2016-09-29 09:38:50 +0530

    asynch

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
-console.log('hello');
-function Car() {
-	//properties
-	this.wheels = 4;
-	this.steering = 1;
-	this.speed = 10;
 
-	//behaviours
-	this.move = function () {
-		console.log('moving...');
-	}
-
-	this.accelarate = function () {
-		this.speed += 10;
-	}
-}
