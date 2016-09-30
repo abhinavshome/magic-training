08e75b6d48cb5eddba75ed01965fcf7b1502652e
Kumar Abhinav <kumar.abhinav@aline-consulting.com>
2016-09-30 10:35:18 +0530

    add to cart

--- app/app.js
+++ app/app.js
@@ -1,26 +1,64 @@
 var booksCart = angular.module('booksCart', []);
 
-booksCart.controller('bookCtrl', function ($scope) {
+booksCart.controller('bookCtrl', function($scope) {
+    $scope.cart = {
+        totalPrice: 0,
+        items: []
+    };
+
     $scope.books = [{
-            title: 'The Alcheimst',
-            author: 'Paulo Cohelo',
-            price: 30,
-            rating: 5
-        }, {
-            title: 'The monk who sold his ferrari',
-            author: 'Robin Sharma',
-            price: 20,
-            rating: 1
-        }, {
-            title: 'THE NIGHTINGALE',
-            author: 'Kristin Hannah',
-            price: 50,
-            rating: 2
-        }, {
-            title: 'Harry Potter',
-            author: 'J K Rowling',
-            price: 10,
-            rating: 4
-        }];
+        title: 'The Alcheimst',
+        author: 'Paulo Cohelo',
+        price: 30,
+        rating: 5
+    }, {
+        title: 'The monk who sold his ferrari',
+        author: 'Robin Sharma',
+        price: 20,
+        rating: 1
+    }, {
+        title: 'THE NIGHTINGALE',
+        author: 'Kristin Hannah',
+        price: 50,
+        rating: 2
+    }, {
+        title: 'Harry Potter',
+        author: 'J K Rowling',
+        price: 10,
+        rating: 4
+    }];
+
+    $scope.rateUp = function(book) {
+        book.rating < 5 && book.rating++
+    };
+
+    $scope.rateDown = function(book) {
+        book.rating > 1 && book.rating--;
+    };
+
+    $scope.addToCart = function (book) {
+        var item = findInCart(book.title);
+        if(!item) {
+            item = {
+                title: book.title,
+                qty: 1,
+                price: book.price
+            };
+            $scope.cart.items.push(item);
+        } else {
+            item.qty++;
+        }
+        $scope.cart.totalPrice += book.price;
+    };
+
+    function findInCart(title) {
+        var foundItems = $scope.cart.items.filter(function (item) {
+            return item.title == title;
+        });
+        return foundItems[0];
+    }
 })
+
+
+
 
